54dc2b81c760320ba846e742ca0c090e9bde4033
Abhay Ratnakar <a.s.ratnakar2192@gmail.com>
2024-06-12 01:19:27 +0530

    this is about 12th of june and we did owner create route complete and now it's time to do a next things

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const ownersRouter = require("./routes/ownersRouter")
+const productsRouter = require("./routes/productsRouter")
+const usersRouter = require("./routes/usersRouter")
+const db = require("./config/mongoose-connection");
+
+
 const app = express();
 
-const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname , "public")));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.send("hey");
-})
+
+
+app.use('/owners', ownersRouter);
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 app.listen(3000);
--- models/ownersRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ownerModel = require("../models/owners-model")
-
-router.get('/', function(req, res) {
-    res.send("hey it's working");
-});
-
-module.exports = router;
